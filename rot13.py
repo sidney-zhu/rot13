f7db6000dc9d0ca09a0611735d63f45ed64e4839
sidney-zhu <sidney.zhu@gmail.com>
2019-12-20 21:16:22 +0800

    First version of ROT13 funciton

---
+++ rot13.py
@@ -0,0 +1,17 @@
+from sys import argv
+
+fn = argv[1]
+
+def rot13(filename):
+
+    with open(filename) as f:
+        d = {}
+        for c in (65, 97):
+            for i in range(26):
+                d[chr(i+c)] = chr((i+13) % 26 + c)
+        s = f.read()
+        
+        print("".join([d.get(c, c) for c in s]))
+
+
+rot13(fn)
